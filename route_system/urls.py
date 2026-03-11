0b1534a1d4d1bebb8f31b214868b3af8daf06ba9
11246004 <11246004@ntub.edu.tw>
2026-03-11 17:02:25 +0800

    Add files via upload

--- route_system/urls.py
+++ route_system/urls.py
@@ -1,22 +1,22 @@
-"""
-URL configuration for route_system project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from routing import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    path("", views.home, name="home"),
+    path("run/", views.run_scheduler, name="run_scheduler"),
+
+    path("api/routes/options/", views.api_route_options, name="api_route_options"),
+    path("api/routes/detail/", views.api_route_detail, name="api_route_detail"),
+    path("api/routes/old-options/", views.api_old_route_options, name="api_old_route_options"),
+    path("api/routes/old-detail/", views.api_old_route_detail, name="api_old_route_detail"),
+    path("api/points/page/", views.api_points_page, name="api_points_page"),
+
+    path("data/", views.data_list, name="data_list"),
+    path("data/add/", views.data_add, name="data_add"),
+    path("data/edit/<int:pk>/", views.data_edit, name="data_edit"),
+    path("data/delete/<int:pk>/", views.data_delete, name="data_delete"),
+    path("data/import/", views.data_import, name="data_import"),
 ]
