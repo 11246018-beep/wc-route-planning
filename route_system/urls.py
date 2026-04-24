ab8f50414f4f8a5d43d41a0f14381763cb7a4341
11246018-beep <11246018@ntub.edu.tw>
2026-04-24 10:21:51 +0800

    Add files via upload

--- route_system/urls.py
+++ route_system/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from routing import account_api, live_api, mobile_api, views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,6 +16,8 @@ urlpatterns = [
     path("live-monitor/", TemplateView.as_view(template_name="routing/live_monitor.html"), name="live_monitor_page"),
     path("run/", views.run_scheduler, name="run_scheduler"),
     path("register/", views.register_view, name="register"),
+    path("cleaning-records/", views.cleaning_records_page, name="cleaning_records_page"),
+    path("cleaning-report/", views.cleaning_report_page),
 
     path("api/routes/options/", views.api_route_options, name="api_route_options"),
     path("api/routes/detail/", views.api_route_detail, name="api_route_detail"),
@@ -28,11 +33,15 @@ urlpatterns = [
     path("api/driver/report/delete/", mobile_api.driver_report_delete_api, name="driver_report_delete_api"),
     path("api/driver/profile/", account_api.driver_profile_api, name="driver_profile_api"),
     path("api/driver/live/update/", live_api.driver_live_update_api, name="driver_live_update_api"),
+    path("api/ai/detect/", mobile_api.detect_cleaning_ai_api, name="detect_cleaning_ai_api"),
 
     path("api/admin/drivers/", account_api.admin_drivers_api, name="admin_drivers_api"),
     path("api/admin/driver/save/", account_api.admin_driver_save_api, name="admin_driver_save_api"),
     path("api/admin/driver/password/", account_api.admin_driver_password_api, name="admin_driver_password_api"),
+    path("api/admin/driver/delete/", account_api.admin_driver_delete_api, name="admin_driver_delete_api"),
     path("api/admin/live/overview/", live_api.admin_live_overview_api, name="admin_live_overview_api"),
+    path("api/admin/cleaning-records/", mobile_api.admin_cleaning_records_api, name="admin_cleaning_records_api"),
+    path("api/admin/cleaning-summary/", mobile_api.admin_cleaning_summary_api, name="admin_cleaning_summary_api"),
 
     path("api/export/excel/", mobile_api.export_excel_api, name="export_excel_api"),
 
@@ -42,3 +51,4 @@ urlpatterns = [
     path("data/delete/<int:pk>/", views.data_delete, name="data_delete"),
     path("data/import/", views.data_import, name="data_import"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
